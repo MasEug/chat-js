1e82a2f1267302b92a5054048bc7cc5857110299
maseug <mas.eug@yandex.ru>
2021-07-11 17:12:40 +0300

    test

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(http);
+
+const port = process.env.PORT || 3000;
+
+users = [];
+connections = [];
+
+// io.on('connection', (socket) => {
+//
+//     console.log('a user connected');
+//
+//     connections.push(socket);
+//
+//     socket.on('disconnect', (data) => {
+//         connections.splice(connections.indexOf(socket), 1)
+//         console.log('off');
+//     })
+// });
+//
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+    console.log('a user connected');
+
+    connections.push(socket);
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1)
+        console.log('off');
+    })
+
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
